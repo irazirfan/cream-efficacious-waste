4d8b447fe48d6c414a38d1f091a6b57f0eeca557
Glitch (cream-efficacious-waste) <none>
2020-04-09 22:46:39 +0000

    🌘🚀 find, remove & chain query

--- myApp.js
+++ myApp.js
@@ -56,137 +56,63 @@ var findOneByFood = function(food, done) {
   });
 };
 
-/** 7) Use `Model.findById()` */
-
-// When saving a document, mongodb automatically add the field `_id`,
-// and set it to a unique alphanumeric key. Searching by `_id` is an
-// extremely frequent operation, so `moongose` provides a dedicated
-// method for it. Find the (only!!) person having a certain Id,
-// using `Model.findById() -> Person`.
-// Use the function argument 'personId' as search key.
-
 var findPersonById = function(personId, done) {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
-
-/** # CR[U]D part III - UPDATE # 
-/*  ============================ */
-
-/** 8) Classic Update : Find, Edit then Save */
-
-// In the good old days this was what you needed to do if you wanted to edit
-// a document and be able to use it somehow e.g. sending it back in a server
-// response. Mongoose has a dedicated updating method : `Model.update()`,
-// which is directly binded to the low-level mongo driver.
-// It can bulk edit many documents matching certain criteria, but it doesn't
-// pass the edited document to its callback, only a 'status' message.
-// Furthermore it makes validation difficult, because it just
-// direcly calls the mongodb driver.
-
-// Find a person by Id ( use any of the above methods ) with the parameter
-// `personId` as search key. Add "hamburger" to the list of her `favoriteFoods`
-// (you can use Array.push()). Then - **inside the find callback** - `.save()`
-// the updated `Person`.
-
-// [*] Hint: This may be tricky if in your `Schema` you declared
-// `favoriteFoods` as an `Array` without specifying the type (i.e. `[String]`).
-// In that case `favoriteFoods` defaults to `Mixed` type, and you have to
-// manually mark it as edited using `document.markModified('edited-field')`
-// (http://mongoosejs.com/docs/schematypes.html - #Mixed )
 
 var findEditThenSave = function(personId, done) {
   var foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) {
+      done(err);
+    }
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err, data) => {
+      if (err) done(err);
+      done(null, data);
+    });
+  });
 };
-
-/** 9) New Update : Use `findOneAndUpdate()` */
-
-// Recent versions of `mongoose` have methods to simplify documents updating.
-// Some more advanced features (i.e. pre/post hooks, validation) beahve
-// differently with this approach, so the 'Classic' method is still useful in
-// many situations. `findByIdAndUpdate()` can be used when searching by Id.
-//
-// Find a person by `name` and set her age to `20`. Use the function parameter
-// `personName` as search key.
-//
-// Hint: We want you to return the **updated** document. In order to do that
-// you need to pass the options document `{ new: true }` as the 3rd argument
-// to `findOneAndUpdate()`. By default the method
-// passes the unmodified object to its callback.
 
 var findAndUpdate = function(personName, done) {
   var ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { $set: { age: ageToSet } },
+    { new: true, useNewUrlParser: true, useUnifinedTopology: true },
+    (err, data) => {
+      if (err) return done(err, data);
+      return done(null, data);
+    }
+  );
 };
-
-/** # CRU[D] part IV - DELETE #
-/*  =========================== */
-
-/** 10) Delete one Person */
-
-// Delete one person by her `_id`. You should use one of the methods
-// `findByIdAndRemove()` or `findOneAndRemove()`. They are similar to the
-// previous update methods. They pass the removed document to the cb.
-// As usual, use the function argument `personId` as search key.
 
 var removeById = function(personId, done) {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if (err) done(err);
+    done(null, data);
+  });
 };
-
-/** 11) Delete many People */
-
-// `Model.remove()` is useful to delete all the documents matching given criteria.
-// Delete all the people whose name is "Mary", using `Model.remove()`.
-// Pass to it a query ducument with the "name" field set, and of course a callback.
-//
-// Note: `Model.remove()` doesn't return the removed document, but a document
-// containing the outcome of the operation, and the number of items affected.
-// Don't forget to pass it to the `done()` callback, since we use it in tests.
 
 var removeManyPeople = function(done) {
   var nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.deleteMany({ name: nameToRemove }, (err, data) => {
+    if (err) done(err);
+    done(null, data);
+  });
 };
-
-/** # C[R]UD part V -  More about Queries # 
-/*  ======================================= */
-
-/** 12) Chain Query helpers */
-
-// If you don't pass the `callback` as the last argument to `Model.find()`
-// (or to the other similar search methods introduced before), the query is
-// not executed, and can even be stored in a variable for later use.
-// This kind of object enables you to build up a query using chaining syntax.
-// The actual db search is executed when you finally chain
-// the method `.exec()`, passing your callback to it.
-// There are many query helpers, here we'll use the most 'famous' ones.
-
-// Find people who like "burrito". Sort them alphabetically by name,
-// Limit the results to two documents, and hide their age.
-// Chain `.find()`, `.sort()`, `.limit()`, `.select()`, and then `.exec()`,
-// passing the `done(err, data)` callback to it.
 
 var queryChain = function(done) {
   var foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch}).sort({ name: "asc" }).limit(2).select("-age").exec((err, data) => {
+      if (err) done(err);
+      done(err, data);
+    });
 };
-
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
-
-/** # Further Readings... #
-/*  ======================= */
-// If you are eager to learn and want to go deeper, You may look at :
-// * Indexes ( very important for query efficiency ),
-// * Pre/Post hooks,
-// * Validation,
-// * Schema Virtuals and  Model, Static, and Instance methods,
-// * and much more in the [mongoose docs](http://mongoosejs.com/docs/)
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
